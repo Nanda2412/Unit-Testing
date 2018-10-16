2e5f98aaafe1b2b293ab33be97d10a0ade901f44
Nanda <tynandu@gmail.com>
2018-10-16 15:34:52 +0530

    Remove ignored files

---
+++ c2/1/build/CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/npuzhank/Documents/Development/Learning/C++/Modern_C++_TDD/CodeBetterSleepBetter/c2/1/SoundexTest.cpp" "/Users/npuzhank/Documents/Development/Learning/C++/Modern_C++_TDD/CodeBetterSleepBetter/c2/1/build/CMakeFiles/test.dir/SoundexTest.cpp.o"
+  "/Users/npuzhank/Documents/Development/Learning/C++/Modern_C++_TDD/CodeBetterSleepBetter/c2/1/main.cpp" "/Users/npuzhank/Documents/Development/Learning/C++/Modern_C++_TDD/CodeBetterSleepBetter/c2/1/build/CMakeFiles/test.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Users/npuzhank/googletest-master/googlemock/include"
+  "/Users/npuzhank/googletest-master/googletest/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
