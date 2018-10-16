d74980b937fe0c14fef5703376d08a5b81f29f26
Nanda <tynandu@gmail.com>
2018-10-16 14:11:13 +0530

    Soundex algorithm Unit Testing - First commit

---
+++ c2/1/SoundexTest.cpp
@@ -0,0 +1,17 @@
+#include "gmock/gmock.h"
+#include "Soundex.h"
+
+using namespace testing;
+
+class SoundexEncoding : public Test{
+public:
+    Soundex soundex;
+};
+
+TEST_F(SoundexEncoding, RetainSoleLetterOfOneLetterWord){
+    ASSERT_THAT(soundex.encode("A"), Eq("A000"));
+}
+
+TEST_F(SoundexEncoding, PadsWithZeroToEnsureThreeDigits){
+    ASSERT_THAT(soundex.encode("I"), Eq("I000")); 
+}
